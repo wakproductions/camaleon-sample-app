bdf9001f75fc1ba9df8a67acfa98b8fb26e06cf8
Winston Kotzan <winston.kotzan@moserit.com>
2020-07-11 15:40:11 -0400

    rails g camaleon_cms:install

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+// Camaleon CMS assets
+//= link camaleon-cms.js
+//= link_tree ../../apps/themes/camaleon_first/assets
+//= link_tree ../../apps/themes/default/assets
+//= link_tree ../../apps/themes/new/assets
